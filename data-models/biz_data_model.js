c75ad0d8934cfac9519cb834ff5c439240b2a910
anthonypiazza <piazza.anthony.n@gmail.com>
2019-08-28 20:28:11 -0500

    Response for PUT, DELETE, POST returns user biz array

--- data-models/biz_data_model.js
+++ data-models/biz_data_model.js
@@ -11,13 +11,38 @@ module.exports = {
     addTerms
 }
 
-function addBiz(newBiz, userID){
+function addBiz(newBiz, userID, username){
     return db('businesses')
         .insert({name: newBiz.name})
         .then(newBizId => {
             console.log(newBizId)
             return db('users-businesses')
                 .insert({ user_id: userID, business_id: newBizId[0], yelp_url: newBiz.yelp_url})
+        })
+        .then(added => {
+            return db('users')   //query user table //in a where if not referencing the top table, (table_name.key)    
+            .join('users-businesses', 'users.id', 'users-businesses.user_id') //join with userbiz intermediary table
+            .join('businesses', 'users-businesses.business_id', 'businesses.id') //join with biz table
+            .select('businesses.id', 'businesses.name', 'users-businesses.yelp_url') //select biz id and name
+            .where({username}) //users.username = username //filter biz array for specific user's listings
+            .then(array => { 
+                return db('data') // query terms table
+                    .join('businesses-terms', 'data.id', 'businesses-terms.term_id') //join with bizterms intermediary table
+                    .join('businesses', 'businesses-terms.business_id', 'businesses.id') //join with biz table 
+                    .select('data.term', 'businesses-terms.highratingscore', 'businesses-terms.poorratingscore', 'businesses-terms.business_id') //select term name, highrating, poorrating, business_id
+                    .then(term_array => {  
+                        const newArray = array.map(biz_object => { //created new Arr with an additional key for each obj -----> ('terms': [termsdata])
+                            const termsInsideBiz = [];
+                            term_array.map(term_object => {
+                                if(biz_object.id === term_object.business_id){
+                                    termsInsideBiz.push(term_object)
+                                }
+                            })
+                            return {...biz_object, data: termsInsideBiz}
+                        })
+                        return newArray;//terms: array of objects of related terms to business
+                    }) 
+            })
         })
 }
 
@@ -53,20 +78,70 @@ function bizDB(){
 }
 
 
-function removeOnlyBiz(id, userId){   
+function removeOnlyBiz(id, userId, username){   
     return db('users-businesses')   //query user table    //array of table 
         .where({user_id: userId, business_id: id}) //filters user_id column for value
-        .del();
+        .del()
+        .then(added => {
+            return db('users')   //query user table //in a where if not referencing the top table, (table_name.key)    
+            .join('users-businesses', 'users.id', 'users-businesses.user_id') //join with userbiz intermediary table
+            .join('businesses', 'users-businesses.business_id', 'businesses.id') //join with biz table
+            .select('businesses.id', 'businesses.name', 'users-businesses.yelp_url') //select biz id and name
+            .where({username}) //users.username = username //filter biz array for specific user's listings
+            .then(array => { 
+                return db('data') // query terms table
+                    .join('businesses-terms', 'data.id', 'businesses-terms.term_id') //join with bizterms intermediary table
+                    .join('businesses', 'businesses-terms.business_id', 'businesses.id') //join with biz table 
+                    .select('data.term', 'businesses-terms.highratingscore', 'businesses-terms.poorratingscore', 'businesses-terms.business_id') //select term name, highrating, poorrating, business_id
+                    .then(term_array => {  
+                        const newArray = array.map(biz_object => { //created new Arr with an additional key for each obj -----> ('terms': [termsdata])
+                            const termsInsideBiz = [];
+                            term_array.map(term_object => {
+                                if(biz_object.id === term_object.business_id){
+                                    termsInsideBiz.push(term_object)
+                                }
+                            })
+                            return {...biz_object, data: termsInsideBiz}
+                        })
+                        return newArray;//terms: array of objects of related terms to business
+                    }) 
+            })
+        })
 }
 // if(businesses.id === par.business_id && businesses.id === id){
 //     .del()
 // }
 
 
-function updateBiz(changes, id, userId){
+function updateBiz(changes, id, userId, username){
     return db('users-businesses')   //query user table    //array of table 
         .where({user_id: userId, business_id: id})//filters user_id column for value
         .update(changes)
+        .then(added => {
+            return db('users')   //query user table //in a where if not referencing the top table, (table_name.key)    
+            .join('users-businesses', 'users.id', 'users-businesses.user_id') //join with userbiz intermediary table
+            .join('businesses', 'users-businesses.business_id', 'businesses.id') //join with biz table
+            .select('businesses.id', 'businesses.name', 'users-businesses.yelp_url') //select biz id and name
+            .where({username}) //users.username = username //filter biz array for specific user's listings
+            .then(array => { 
+                return db('data') // query terms table
+                    .join('businesses-terms', 'data.id', 'businesses-terms.term_id') //join with bizterms intermediary table
+                    .join('businesses', 'businesses-terms.business_id', 'businesses.id') //join with biz table 
+                    .select('data.term', 'businesses-terms.highratingscore', 'businesses-terms.poorratingscore', 'businesses-terms.business_id') //select term name, highrating, poorrating, business_id
+                    .then(term_array => {  
+                        const newArray = array.map(biz_object => { //created new Arr with an additional key for each obj -----> ('terms': [termsdata])
+                            const termsInsideBiz = [];
+                            term_array.map(term_object => {
+                                if(biz_object.id === term_object.business_id){
+                                    termsInsideBiz.push(term_object)
+                                }
+                            })
+                            return {...biz_object, data: termsInsideBiz}
+                        })
+                        return newArray;//terms: array of objects of related terms to business
+                    }) 
+            })
+        })
 }
 
 
